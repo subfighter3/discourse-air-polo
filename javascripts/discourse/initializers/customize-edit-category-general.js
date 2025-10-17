eac53c1b0bd472d0639c7e7cb4f619edce8e11e5
Pyetro <17915306+subfighter3@users.noreply.github.com>
2025-10-17 13:05:05 +0200

    merge with original repo. bump to 1.1.6

--- javascripts/discourse/initializers/customize-edit-category-general.js
+++ javascripts/discourse/initializers/customize-edit-category-general.js
@@ -2,25 +2,25 @@ import { cancel } from "@ember/runloop";
 import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default {
-  name: "customize-edit-category-general",
+	name: "customize-edit-category-general",
 
-  initialize() {
-    withPluginApi("0.8.14", (api) => {
-      api.modifyClass("component:edit-category-general", {
-        pluginId: "discourse-air",
+	initialize() {
+		withPluginApi((api) => {
+			api.modifyClass("component:edit-category-general", {
+				pluginId: "discourse-air",
 
-        didInsertElement() {
-          this._super(...arguments);
-          document.body.classList.add("edit-category");
-          this._focusCategoryName();
-        },
+				didInsertElement() {
+					this._super(...arguments);
+					document.body.classList.add("edit-category");
+					this._focusCategoryName();
+				},
 
-        willDestroyElement() {
-          this._super(...arguments);
-          document.body.classList.remove("edit-category");
-          this._laterFocus && cancel(this._laterFocus);
-        },
-      });
-    });
-  },
+				willDestroyElement() {
+					this._super(...arguments);
+					document.body.classList.remove("edit-category");
+					this._laterFocus && cancel(this._laterFocus);
+				},
+			});
+		});
+	},
 };
